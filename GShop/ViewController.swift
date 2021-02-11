47abcada65d046723f4ecb3c16132ca8c0f60ed6
Sergei Morugin <cmorugin@gmail.com>
2021-02-11 13:48:09 +0300

    init

---
+++ GShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GShop
+//
+//  Created by Matthew on 11.02.2021.
+//  Copyright © 2021 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
