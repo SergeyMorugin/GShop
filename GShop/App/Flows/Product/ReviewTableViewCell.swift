819a21bf5b0ada92149e9df41c8dfe0c1d117999
Sergei Morugin <cmorugin@gmail.com>
2021-03-22 19:34:14 +0300

    add ReviewTableViewCell

---
+++ GShop/App/Flows/Product/ReviewTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  ReviewTableViewCell.swift
+//  GShop
+//
+//  Created by Matthew on 22.03.2021.
+//  Copyright © 2021 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func showReview(review: Review?) {
+        if let review = review {
+            userLabel.text = String(review.userId)
+            reviewLabel.text = review.text
+        } else {
+            userLabel.text = ""
+            reviewLabel.text = ""
+        }
+    }
+    
+}
